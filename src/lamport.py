ca17ec665d13213573b1ae4937884424c4439bc8
Rakshith <hehaichi@gmail.com>
2020-01-20 01:54:45 -0800

    Modify logging in lamport module

--- src/lamport.py
+++ src/lamport.py
@@ -1,12 +1,20 @@
 import logging
 
+logger = logging.getLogger('client')
+
 class LamportClock:
-    def __init__(self, port):
-        self.time = 0
+    def __init__(self, port, time=0):
+        self.time = time
         self.proc_id = port
+        # Logging configuration
+        # need to send create a logging adapter just as we had done in client
+        # otherwise this logger is not compatible with client logger
+        extra = {'port':port}
+        global logger
+        logger = logging.LoggerAdapter(logger, extra)        
     
     def update_time(self, time=0):
         self.time = max(self.time, time) + 1
-        logging.debug("Updated lamport clock to {}".format(self.time))
+        logger.debug("Updated lamport clock to {}".format(self.time))
         return self.time
     
